208a96117066c3440c3f982d11af94692147fc48
Mauricio <mauricio.cleveland@gmail.com>
2021-04-08 16:36:24 -0400

    Added register activity

---
+++ app/app/src/main/java/com/chat/boot/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.chat.boot
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
