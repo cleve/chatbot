212d380a70786baae1038bea6b293fddf360e620
Mauricio <mauricio.cleveland@gmail.com>
2021-04-09 11:12:26 -0400

    Fields creation

--- app/app/src/main/java/com/chat/boot/RegisterActivity.kt
+++ app/app/src/main/java/com/chat/boot/RegisterActivity.kt
@@ -1,6 +1,8 @@
 package com.chat.boot
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class RegisterActivity : AppCompatActivity() {
@@ -9,5 +11,12 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Init values.
+        val email = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val username = findViewById<EditText>(R.id.editTextTextUsername)
+        val password = findViewById<EditText>(R.id.editTextPassword)
+        val buttonRegister = findViewById<Button>(R.id.registerUser)
+
     }
 }
